2c0524c3350c08e96d15b2aea3d6edc70db4f367
skonishi1125 <skonishi1125@gmail.com>
2020-12-28 12:07:35 +0900

    header-wrapperの調整前

--- join/web/index.php
+++ join/web/index.php
@@ -144,6 +144,21 @@ include('../app/_parts/_header.php');
     </div>
   </div>
 
+  <!-- <section class="row resHeader-bgWrapper" style="background-image: url('../img/head<?php echo $random ?>.png')">
+  <div class="header-wrapperLogo">
+    <img src="../img/whiteLogo.png" alt="Liko" >
+    <p>スキを共有しましょう</p>
+  </div>
+</section> -->
+
+
+
+
+
+
+
+
+
   <div class="row header-wrapperRegister">
 
     <form class="mt-2 ml-3" action="" method="post" enctype="multipart/form-data">
@@ -195,6 +210,13 @@ include('../app/_parts/_header.php');
 
   </div> <!-- row header-wrapperRegister -->
 </header>
+
+<section class="row resHeader-bgWrapper" style="background-image: url('../img/head<?php echo $random ?>.png')">
+  <div class="header-wrapperLogo">
+    <img src="../img/whiteLogo.png" alt="Liko" >
+    <p>スキを共有しましょう</p>
+  </div>
+</section>
 
 
 
@@ -292,213 +314,211 @@ include('../app/_parts/_header.php');
   </div>
 </nav>
 
+<div class="test"></div>
 
-
-
-
-
-
-
-
-<section class="container intro-container pb-5">
-  <div class="intro-centerBar py-4"></div>
-  <h4 class="my-3"><b>Likoとは</b></h4>
-
-  <div class="row">
-    
-    <div class="col-md-8">
-      <img src="../img/intro1.png" alt="intro1">
-    </div>
-
-    <div class="col-md-4 intro-containerMessages">
-      <p>Likoでは様々な人たちが「スキ」だと感じたことについての投稿を確認できます。</p>
-      <p>自分の好きなものを投稿して、感想を語り合うことも可能です。</p>
-      <p>好きな場所や動画、食べ物だけでなくキャラクターや有名人など、投稿する内容はユーザーの自由です。<br>プチブログのような使い方も。</p>
-    </div>
-
-  </div>
-
-</section>
-
-<section class="intro-container pb-5 intro-containerBg" style="background-image: url('../img/head<?php print mt_rand_except( 1, 4, $random ); ?>.png')">
-<!-- <section class="intro-container pb-5 intro-containerBg bg-switcher"> -->
-<div class="intro-centerBar py-4 border-white"></div>
-  <h4 class="my-3"><b>できること</b></h4>
-
-  <div class="container">
-    
+<div class="background-white">
+  
+  <section class="container intro-container pb-5">
+    <div class="intro-centerBar py-4"></div>
+    <h4 class="my-3"><b>Likoとは</b></h4>
+  
     <div class="row">
       
-      <div class="col-md-4 intro-containerMessages">
-        <p>自分の好みの投稿に「いいね！」したり、コメントをつけることができます。</p>
-        <p>また、自分と同じものが好きな人を探すことができる検索機能、プロフィールアイコンの変更機能なども。</p>
-        <br>
-        <p>自分の好きなものについて、ユーザーと語り合いましょう！</p>
-      </div>
-  
       <div class="col-md-8">
-        <img src="../img/intro2.png" alt="intro1">
+        <img src="../img/intro1.png" alt="intro1">
       </div>
-
-    </div>
-
-  </div>
-
-</section>
-
-<section class="container intro-container pb-5">
-  <div class="intro-centerBar py-4"></div>
-  <h4 class="my-3"><b>FAQ</b></h4>
-
-  <section id="faq">
-
-    <div class="intro-containerFAQ col-md-12">
-      <h5 data-toggle="collapse" href="#collapseContent01" role="button" aria-expanded="false" aria-controls="collapseContent01" id="toggleIcon1" class="py-3">
-        <b class="ml-3">このサイトについて</b>
-        <i class="fas fa-plus mr-3" id="plus1"></i>
-        <i class="fas fa-minus mr-3 skelton" id="minus1"></i>
-      </h5>
-
-      <div class="collapse" id="collapseContent01">
-        <div class="card card-body my-4">
-          <p>ポートフォリオ用のSNSサイトとなります。</p>
-          <p>突然メンテナンスを行ったり、サービスが停止することがございます。ご了承ください。</p>
-        </div>
-      </div>
-    </div>
-
-    <h2 style="color: white" >　</h2>
   
-    <div class="intro-containerFAQ col-md-12">
-      <h5 data-toggle="collapse" href="#collapseContent02" role="button" aria-expanded="false" aria-controls="collapseContent02" id="toggleIcon2" class="py-3">
-        <b class="ml-3">情報の取扱い</b>
-        <i class="fas fa-plus mr-3" id="plus2"></i>
-        <i class="fas fa-minus mr-3 skelton" id="minus2"></i>
-      </h5>
-
-      <div class="collapse" id="collapseContent02">
-        <div class="card card-body my-4">
-          <p>
-           メールアドレスについては管理者から確認が可能です。<br>
-           メールアドレスはユーザーのログインのみにしか利用せず、管理者がその他の用途に利用することはありません。
-          </p>
-           <p>
-            パスワードはハッシュ関数「sha256」を利用した暗号化形式を採用し保存しています。<br>
-            管理者から確認することはできないような仕組みで管理しています。
-          </p>
-          <p>
-          自分のメールアドレスなどを利用せずにサービスを利用したい場合は、架空のアドレスを登録することでもサービスの利用が可能です。<br>
-          (ログイン時に利用しますので、忘れないように注意してください)
-          </p>
-        </div>
+      <div class="col-md-4 intro-containerMessages">
+        <p>Likoでは様々な人たちが「スキ」だと感じたことについての投稿を確認できます。</p>
+        <p>自分の好きなものを投稿して、感想を語り合うことも可能です。</p>
+        <p>好きな場所や動画、食べ物だけでなくキャラクターや有名人など、投稿する内容はユーザーの自由です。<br>プチブログのような使い方も。</p>
       </div>
-
-    </div>
-
-    <h2 style="color: white" >　</h2>
   
-    <div class="intro-containerFAQ col-md-12">
-      <h5 data-toggle="collapse" href="#collapseContent03" role="button" aria-expanded="false" aria-controls="collapseContent03" id="toggleIcon3" class="py-3">
-        <b class="ml-3">不具合が発生した時</b>
-        <i class="fas fa-plus mr-3" id="plus3"></i>
-        <i class="fas fa-minus mr-3 skelton" id="minus3"></i>
-      </h5>
-
-      <div class="collapse" id="collapseContent03">
-        <div class="card card-body my-4">
-          <p>下記のアドレスまで伝えて頂ければ幸いです。確認が出来次第、返信いたします。</p>
-          <p>email : skonishi1125@gmail.com</p>
-        </div>
-      </div>
-
     </div>
-
-    <h2 style="color: white" >　</h2>
-
+  
   </section>
-
-</section>
-
-
-
-<section class="container intro-container pb-5">
-  <div class="intro-centerBar py-4"></div>
-  <h4 class="my-3"><b>はじめてみよう</b></h4>
-
-  <div class="row pt-4">
-    <div class="col-md-4 noResponsive-registerbutton">
-      <a href="index.php" class="btn btn-primary btn-sm getStartButtons" role="button"><i class="fas mr-1 fa-user-plus"></i>登録する</a>
-    </div>
-
-    <!-- 画面幅767px以下の時に出るボタン -->
-    <div class="col-md-4 responsive-registerbutton">
-      <a href="" role="button" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-sm getStartButtons" role="button"><i class="fas mr-1 fa-user-plus"></i>登録する</a>
-    </div>
-
-    <div class="col-md-4">
-      <a class="btn btn-primary btn-sm getStartButtons" role="button" href="../../web/login.php"><i class="fas mr-1 fa-sign-in-alt"></i>ログインする</a>
-    </div>
-
-    <div class="col-md-4">
-      <a class="btn btn-success btn-sm getStartButtons" role="button" href="../../app/testLogin.php"><i class="fas mr-1 fa-sign-in-alt"></i>お試しログイン</a>
-    </div>
-
-  </div>
-
-  <div class="row mt-5 getStartNote">
-    <div class="col-md-12">
-      <h6 class="pb-2">お試しログイン機能について</h6>
-      <p>ユーザー登録作業をスキップし、テストアカウントでLikoへログインすることができます。</p>
-      <p>Likoの仕組みを確認したい場合はこちらでのログインをお試しください。</p>
-    </div>
-
-  </div>
   
-</section>
-
-<section class="container intro-container pb-5">
-  <div class="intro-centerBar py-4"></div>
-  <h4 class="my-3"><b>製作者プロフィール</b></h4>
-
-  <div class="row profile pt-3">
-    <div class="col-md-6">
-      <img class="img-thumbnail" src="../img/profile.png" alt="profile">
+  <section class="intro-container pb-5 intro-containerBg" style="background-image: url('../img/head<?php print mt_rand_except( 1, 4, $random ); ?>.png')">
+  <!-- <section class="intro-container pb-5 intro-containerBg bg-switcher"> -->
+  <div class="intro-centerBar py-4 border-white"></div>
+    <h4 class="my-3"><b>できること</b></h4>
+  
+    <div class="container">
+      
+      <div class="row">
+        
+        <div class="col-md-4 intro-containerMessages">
+          <p>自分の好みの投稿に「いいね！」したり、コメントをつけることができます。</p>
+          <p>また、自分と同じものが好きな人を探すことができる検索機能、プロフィールアイコンの変更機能なども。</p>
+          <br>
+          <p>自分の好きなものについて、ユーザーと語り合いましょう！</p>
+        </div>
+    
+        <div class="col-md-8">
+          <img src="../img/intro2.png" alt="intro1">
+        </div>
+  
+      </div>
+  
     </div>
-
-    <div class="col-md-6 profileTexts mt-3">
-      <h6>名前</h6>
-      <p>小西 慧(Satoru Konishi)<br>
-      1996年11月25日生まれ (2020年11月現在:24歳) </p>
-      <h6>趣味</h6>
-      <p>イラスト描画、ランニング、eSports</p>
-      <h6>SNS</h6>
-      <ul>
-          <li>
-            <p>Qiita<br>
-            <a href="https://qiita.com/skonishi1125">https://qiita.com/skonishi1125</a></p>
-          </li>
-          <li>
-            <p>Github<br>
-            <a href="https://github.com/skonishi1125/liko_re">https://github.com/skonishi1125/liko_re</a></p>
-          </li>
-        </ul>
+  
+  </section>
+  
+  <section class="container intro-container pb-5">
+    <div class="intro-centerBar py-4"></div>
+    <h4 class="my-3"><b>FAQ</b></h4>
+  
+    <section id="faq">
+  
+      <div class="intro-containerFAQ col-md-12">
+        <h5 data-toggle="collapse" href="#collapseContent01" role="button" aria-expanded="false" aria-controls="collapseContent01" id="toggleIcon1" class="py-3">
+          <b class="ml-3">このサイトについて</b>
+          <i class="fas fa-plus mr-3" id="plus1"></i>
+          <i class="fas fa-minus mr-3 skelton" id="minus1"></i>
+        </h5>
+  
+        <div class="collapse" id="collapseContent01">
+          <div class="card card-body my-4">
+            <p>ポートフォリオ用のSNSサイトとなります。</p>
+            <p>突然メンテナンスを行ったり、サービスが停止することがございます。ご了承ください。</p>
+          </div>
+        </div>
+      </div>
+  
+      <h2 style="color: white" >　</h2>
+    
+      <div class="intro-containerFAQ col-md-12">
+        <h5 data-toggle="collapse" href="#collapseContent02" role="button" aria-expanded="false" aria-controls="collapseContent02" id="toggleIcon2" class="py-3">
+          <b class="ml-3">情報の取扱い</b>
+          <i class="fas fa-plus mr-3" id="plus2"></i>
+          <i class="fas fa-minus mr-3 skelton" id="minus2"></i>
+        </h5>
+  
+        <div class="collapse" id="collapseContent02">
+          <div class="card card-body my-4">
+            <p>
+             メールアドレスについては管理者から確認が可能です。<br>
+             メールアドレスはユーザーのログインのみにしか利用せず、管理者がその他の用途に利用することはありません。
+            </p>
+             <p>
+              パスワードはハッシュ関数「sha256」を利用した暗号化形式を採用し保存しています。<br>
+              管理者から確認することはできないような仕組みで管理しています。
+            </p>
+            <p>
+            自分のメールアドレスなどを利用せずにサービスを利用したい場合は、架空のアドレスを登録することでもサービスの利用が可能です。<br>
+            (ログイン時に利用しますので、忘れないように注意してください)
+            </p>
+          </div>
+        </div>
+  
+      </div>
+  
+      <h2 style="color: white" >　</h2>
+    
+      <div class="intro-containerFAQ col-md-12">
+        <h5 data-toggle="collapse" href="#collapseContent03" role="button" aria-expanded="false" aria-controls="collapseContent03" id="toggleIcon3" class="py-3">
+          <b class="ml-3">不具合が発生した時</b>
+          <i class="fas fa-plus mr-3" id="plus3"></i>
+          <i class="fas fa-minus mr-3 skelton" id="minus3"></i>
+        </h5>
+  
+        <div class="collapse" id="collapseContent03">
+          <div class="card card-body my-4">
+            <p>下記のアドレスまで伝えて頂ければ幸いです。確認が出来次第、返信いたします。</p>
+            <p>email : skonishi1125@gmail.com</p>
+          </div>
+        </div>
+  
+      </div>
+  
+      <h2 style="color: white" >　</h2>
+  
+    </section>
+  
+  </section>
+  
+  
+  
+  <section class="container intro-container pb-5">
+    <div class="intro-centerBar py-4"></div>
+    <h4 class="my-3"><b>はじめてみよう</b></h4>
+  
+    <div class="row pt-4">
+      <div class="col-md-4 noResponsive-registerbutton">
+        <a href="index.php" class="btn btn-primary btn-sm getStartButtons" role="button"><i class="fas mr-1 fa-user-plus"></i>登録する</a>
+      </div>
+  
+      <!-- 画面幅767px以下の時に出るボタン -->
+      <div class="col-md-4 responsive-registerbutton">
+        <a href="" role="button" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-sm getStartButtons" role="button"><i class="fas mr-1 fa-user-plus"></i>登録する</a>
+      </div>
+  
+      <div class="col-md-4">
+        <a class="btn btn-primary btn-sm getStartButtons" role="button" href="../../web/login.php"><i class="fas mr-1 fa-sign-in-alt"></i>ログインする</a>
+      </div>
+  
+      <div class="col-md-4">
+        <a class="btn btn-success btn-sm getStartButtons" role="button" href="../../app/testLogin.php"><i class="fas mr-1 fa-sign-in-alt"></i>お試しログイン</a>
+      </div>
+  
+    </div>
+  
+    <div class="row mt-5 getStartNote">
+      <div class="col-md-12">
+        <h6 class="pb-2">お試しログイン機能について</h6>
+        <p>ユーザー登録作業をスキップし、テストアカウントでLikoへログインすることができます。</p>
+        <p>Likoの仕組みを確認したい場合はこちらでのログインをお試しください。</p>
+      </div>
+  
     </div>
     
-  </div>
+  </section>
+  
+  <section class="container intro-container pb-5">
+    <div class="intro-centerBar py-4"></div>
+    <h4 class="my-3"><b>製作者プロフィール</b></h4>
+  
+    <div class="row profile pt-3">
+      <div class="col-md-6">
+        <img class="img-thumbnail" src="../img/profile.png" alt="profile">
+      </div>
+  
+      <div class="col-md-6 profileTexts mt-3">
+        <h6>名前</h6>
+        <p>小西 慧(Satoru Konishi)<br>
+        1996年11月25日生まれ (2020年11月現在:24歳) </p>
+        <h6>趣味</h6>
+        <p>イラスト描画、ランニング、eSports</p>
+        <h6>SNS</h6>
+        <ul>
+            <li>
+              <p>Qiita<br>
+              <a href="https://qiita.com/skonishi1125">https://qiita.com/skonishi1125</a></p>
+            </li>
+            <li>
+              <p>Github<br>
+              <a href="https://github.com/skonishi1125/liko_re">https://github.com/skonishi1125/liko_re</a></p>
+            </li>
+          </ul>
+      </div>
+      
+    </div>
+  
+  </section>
+  
+  
+  
+  
+  <footer class="py-2">
+    <div class="footer-logo">
+      <img src="../img/whiteLogo.png" alt="liko" class="mt-2">
+    </div>
+    <div class="footer-container">
+      <p>2020-2021 ©︎Satoru Konishi.</p>
+    </div>
+  </footer>
 
-</section>
-
-
-
-
-<footer class="py-2">
-  <div class="footer-logo">
-    <img src="../img/whiteLogo.png" alt="liko" class="mt-2">
-  </div>
-  <div class="footer-container">
-    <p>2020-2021 ©︎Satoru Konishi.</p>
-  </div>
-</footer>
+</div> <!-- background-white  -->
 
 
 
