37b285b7ef5f3398632e682875996c7ced748990
skonishi1125 <skonishi1125@gmail.com>
2021-01-13 19:28:15 +0900

    ログアウト時、cookieが消去されるように修正 ページネーションとコメントのmarginができるよう修正

--- app/logout.php
+++ app/logout.php
@@ -11,30 +11,9 @@ session_destroy();
 
 
 //クッキーの削除
-// setcookie('sessionid', '', time()-3600, '/');
-
-// setcookie('test', $test, time() + 3600, '/');
-// $test = 'aaa';
-// setcookie('test', $test, time() + 3600, '/');
-
-
-// unset($_COOKIE['sessionid']);
-
-//なぜか2回読み込まないと飛ばない理由を探る
-//login.phpに飛んだ時にどこからcookie[test]を拾ってきているのか確認する
-
-setcookie('email', '', time() - 3600, '/liko_201223/web');
-setcookie('password', '', time() - 3600, '/liko_201223/web');
-
+setcookie('sessionid', '', time()-3600, '/liko_201223/web');
 
 header('Location: http://localhost:8888/liko_201223/web/login.php');
 exit();
-
-if ($_COOKIE['test'] == 'aaa') {
-  header('Location: http://localhost:8888/liko_201223/join/web/index.php');
-  exit();
-  echo 'aaaがcookieに保管されています';
-}
-echo "\n" . 'うおお';
 
 ?>
