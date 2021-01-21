8d9d45cb977959a503db2759dc809b0e78a223a5
skonishi1125 <skonishi1125@gmail.com>
2021-01-22 00:01:06 +0900

    微修正

--- js/ajaxGoods.js
+++ js/ajaxGoods.js
@@ -2,15 +2,15 @@ $(function() {
   // console.log('接続テスト');
 
   $('.contents-cancelGoodBtn').click(function() {
-    var $likediv =$(this).parent("div");
-    ccGB_postid = $(this).data('postid');
-    ccGB_memberid = $(this).data('memberid');
-    $ccGB_goodNumberSpace = $likediv.find('.ccGB_goodNumberSpace');
-    $caGB_goodNumberSpace = $likediv.find('.caGB_goodNumberSpace');
-    $ccGB_btn = $likediv.find('.contents-cancelGoodBtn');
-    $caGB_btn = $likediv.find('.contents-addGoodBtn');
+    let $likediv =$(this).parent("div");
+    let ccGB_postid = $(this).data('postid');
+    let ccGB_memberid = $(this).data('memberid');
+    let $ccGB_goodNumberSpace = $likediv.find('.ccGB_goodNumberSpace');
+    let $caGB_goodNumberSpace = $likediv.find('.caGB_goodNumberSpace');
+    let $ccGB_btn = $likediv.find('.contents-cancelGoodBtn');
+    let $caGB_btn = $likediv.find('.contents-addGoodBtn');
 
-    console.log(ccGB_postid);
+    // console.log(ccGB_postid);
     
     $.post('../app/subGoods.php', {
       postid: ccGB_postid,
@@ -27,15 +27,15 @@ $(function() {
   });
 
   $('.contents-addGoodBtn').click(function() {
-    var $likediv =$(this).parent("div");
-    caGB_postid = $(this).data('postid');
-    caGB_memberid = $(this).data('memberid');
-    $ccGB_goodNumberSpace = $likediv.find('.ccGB_goodNumberSpace');
-    $caGB_goodNumberSpace = $likediv.find('.caGB_goodNumberSpace');
-    $ccGB_btn = $likediv.find('.contents-cancelGoodBtn');
-    $caGB_btn = $likediv.find('.contents-addGoodBtn');
+    let $likediv =$(this).parent("div");
+    let caGB_postid = $(this).data('postid');
+    let caGB_memberid = $(this).data('memberid');
+    let $ccGB_goodNumberSpace = $likediv.find('.ccGB_goodNumberSpace');
+    let $caGB_goodNumberSpace = $likediv.find('.caGB_goodNumberSpace');
+    let $ccGB_btn = $likediv.find('.contents-cancelGoodBtn');
+    let $caGB_btn = $likediv.find('.contents-addGoodBtn');
 
-    console.log(caGB_postid);
+    // console.log(caGB_postid);
 
     $.post('../app/addGoods.php', {
       postid: caGB_postid,
